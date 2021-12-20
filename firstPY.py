e03c9016e81f7b06cd835047cc3167cf396f6536
Frank Lam <franklam2008@gmail.com>
2021-12-20 18:51:12 -0500

    first commit 2

---
+++ firstPY.py
@@ -0,0 +1,17 @@
+# test = 10
+# trueFalse = True
+# print(test)
+# print(trueFalse)
+# print("trueFalse")
+
+friends=['Jim','Karen']
+print(friends)
+
+test= friends
+
+print(test)
+
+def sayHi():
+    print('Hello User')
+
+sayHi() 
